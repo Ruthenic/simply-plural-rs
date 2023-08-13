4316e6c33e50ff6b29954586a07664d718db2633
Ruthenic <mdrakea3@tutanota.com>
2023-08-13 12:35:32 -0400

    formatting cleanup + minimarl readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# simply-plural-rs
+WIP wrapper around the Simply Plural API.
+
+## Documentation
+Does not exist.
+
+## Examples
+Refer to [the tests](https://github.com/Ruthenic/simply-plural-rs/tree/master/tests)
--- tests/1_user.rs
+++ tests/1_user.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use std::env::var;
+    use std::env;
 
     use rand::prelude::*;
     use simply_plural::*;
@@ -10,25 +10,19 @@ mod tests {
         let mut rng = rand::thread_rng();
 
         let api = SPApi::new(
-            var("SP_API_KEY").expect("No api key specified!"),
+            env::var("SP_API_KEY").expect("No api key specified!"),
             SPEndpoint::Pretesting,
         )
         .expect("Failed to create SPApi struct!");
 
         let mut user = api.get_user(None).await.unwrap();
-
-        dbg!(&user);
-
         user.set_username(format!("APITest{}", rng.gen_range(0..i32::MAX)))
             .await
             .expect("Failed to change username!");
-
         user.is_a_system = !user.is_a_system;
 
         let field = &mut user.get_fields()[0];
-
         field.1.name = format!("{}_{}", field.0, rng.gen_range(0..i32::MAX));
-
         user.set_field(field.0.clone(), field.1.clone());
 
         user.update().await.expect("Failed to update user!")
